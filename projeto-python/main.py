69212a8d89dd0e36a8cb8f636721b8dd8395bdb7
ronaldodeschain <lotushall@gmail.com>
2025-06-05 21:27:58 -0300

    criando bloco de notas com tkinter

--- projeto-python/main.py
+++ projeto-python/main.py
@@ -1,23 +1,47 @@
-import mensagens
-import funcoes
+import tkinter as tk
+import os
+from tkinter.filedialog import askopenfilename, asksaveasfile
 
-while True:
-   mensagens.boas_vindas()
-   mensagens.menu()
-   opcao = input("\n >>> ")
+def abre_arquivo():
+    arq_caminho = askopenfilename()
+    print(f'{arq_caminho= }')
+    with open(arq_caminho,mode='r',encoding='utf-8') as arq:
+        conteudo = ''
+        for linha in arq.readlines():
+            conteudo += linha
 
-   if opcao == '0':
-      mensagens.adios()
-      break   
-   if opcao == '1':
-      funcoes.adiciona_contato()
-   if opcao == '2':
-      funcoes.busca_contato() 
-   if opcao == '3':
-      funcoes.alterar_contato()
-   if opcao == '4':
-      funcoes.apaga_contato()
-   else:
-      print("errado isso ai manin")  
-   input()
-   
+    caixa_texto.delete('1.0','end')
+    caixa_texto.insert('1.0',conteudo)
+
+def novo_arquivo():
+    caixa_texto.delete('1.0','end')
+
+def salva_arquivo():
+    conteudo = caixa_texto.get('1.0','end')
+    print(f'{conteudo= }')
+    arquivo = asksaveasfile()
+    arquivo.write(conteudo)
+    arquivo.close()
+janela = tk.Tk()
+janela.title('uma janela, azul caneta')
+janela.geometry('400x400')
+janela.resizable(False,False)
+
+#widgets
+#menu, Text area
+menu_principal = tk.Menu(janela)
+janela.config(menu=menu_principal)
+
+menu_arquivo = tk.Menu(menu_principal,tearoff=0)
+menu_principal.add_cascade(label='arquivo',menu=menu_arquivo)
+menu_arquivo.add_command(label='Novo',command=novo_arquivo)
+menu_arquivo.add_command(label='Abrir',command=abre_arquivo)
+menu_arquivo.add_command(label='Salvar',command=salva_arquivo)
+menu_arquivo.add_separator()
+menu_arquivo.add_command(label='Sair',command=janela.quit)
+
+caixa_texto = tk.Text(janela,font=('Arial',23))
+caixa_texto.pack()
+
+
+janela.mainloop()
